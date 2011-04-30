74ca8f9b2c43c9eacd8502de1c49759bbc1a6e63
Dave Kilian <dave@fracture-studios.com>
2011-04-30 00:38:36 -0400

    That should be all the busy work for now -_-'

--- AndyNative/jni/agl.c
+++ AndyNative/jni/agl.c
@@ -4,3 +4,338 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "math.h"
+
+void  aglInitialize2D(GLint w, GLint h)
+{
+
+}
+
+void  aglSetVirtualDimensions(GLint w, GLint h)
+{
+
+}
+
+void  aglGetVirtualTransform(GLfloat *m)
+{
+
+}
+
+void  aglSetVirtualTransform(GLfloat *m)
+{
+
+}
+
+void  aglComputeVirtualTransform()
+{
+
+}
+
+GLint aglLoadShader(const char* vertex, const char* fragment)
+{
+	return -1;
+}
+
+void  aglUseShader(GLint shader)
+{
+
+}
+
+void  aglClearShader()
+{
+
+}
+
+void  aglDeleteShader(GLint shader)
+{
+
+}
+
+GLint aglCreateTexture(GLint w, GLint h)
+{
+	return -1;
+}
+
+GLint aglCreateEmptyTexture()
+{
+	return -1;
+}
+
+void  aglBindTexture(GLint tex)
+{
+
+}
+
+void  aglUnbindTexture()
+{
+
+}
+
+void  aglDeleteTexture(GLint tex)
+{
+
+}
+
+void  aglTexturedQuad()
+{
+
+}
+
+void  aglDrawBitmap(GLint tex)
+{
+
+}
+
+void  aglDrawBitmapTranslated(GLint tex, GLfloat x, GLfloat y)
+{
+
+}
+
+void  aglDrawBitmapTransformed(GLint tex, GLfloat x, GLfloat y, GLfloat rot, GLfloat xscale, GLfloat yscale)
+{
+
+}
+
+void  aglDrawBitmapWithShader(GLint tex, GLint shader)
+{
+
+}
+
+void  aglDrawBitmapWithShaderTranslated(GLint tex, GLint shader, GLfloat x, GLfloat y)
+{
+
+}
+
+void  aglDrawBitmapWithShaderTransformed(GLint tex, GLint shader, GLfloat x, GLfloat y, GLfloat rot, GLfloat xscale, GLfloat yscale)
+{
+
+}
+
+void  aglDrawBitmapWithoutShader(GLint tex)
+{
+
+}
+
+void  aglDrawBitmapWithoutShaderTranslated(GLint tex, GLfloat x, GLfloat y)
+{
+
+}
+
+void  aglDrawBitmapWithoutShaderTransformed(GLint tex, GLfloat x, GLfloat y, GLfloat rot, GLfloat xscale, GLfloat yscale)
+{
+
+}
+
+void  aglClearColor(GLfloat r, GLfloat g, GLfloat b)
+{
+
+}
+
+void  aglBeginFrame()
+{
+
+}
+
+void  aglEndFrame()
+{
+
+}
+
+GLint aglCreateFBO(GLint w, GLint h)
+{
+	return -1;
+}
+
+void  aglAttachToFBO(GLint fbo, GLint tex)
+{
+
+}
+
+void  aglDeleteFBO(GLint fbo)
+{
+
+}
+
+void  aglBeginOffscreenRender(GLint fbo)
+{
+
+}
+
+void  aglEndOffscreenRender()
+{
+
+}
+
+
+void Java_dkilian_andy_jni_agl_Initialize2D(JNIEnv *env, jobject *thiz, jint w, jint h)
+{
+	aglInitialize2D(w, h);
+}
+
+void Java_dkilian_andy_jni_agl_SetVirtualDimensions(JNIEnv *env, jobject *thiz, jint w, jint h)
+{
+	aglSetVirtualDimensions(w, h);
+}
+
+void Java_dkilian_andy_jni_agl_GetVirtualTransform(JNIEnv *env, jobject *thiz, jfloatArray mat)
+{
+	float m[16];
+	aglGetVirtualTransform(m);
+	(*env)->SetFloatArrayRegion(env, mat, 0, 16, &(m[0]));
+}
+
+void Java_dkilian_andy_jni_agl_SetVirtualTransfrom(JNIEnv *env, jobject *thiz, jfloatArray mat)
+{
+	float *m = (*env)->GetFloatArrayElements(env, mat, NULL);
+	aglSetVirtualTransform(m);
+	(*env)->ReleaseFloatArrayElements(env, mat, m, JNI_ABORT);
+}
+
+void Java_dkilian_andy_jni_agl_ComputeVirtualTransform(JNIEnv *env, jobject *thiz)
+{
+	aglComputeVirtualTransform();
+}
+
+jint Java_dkilian_andy_jni_agl_LoadShader(JNIEnv *env, jobject *thiz, jstring vertex, jstring fragment)
+{
+	const char* v, *f;
+
+	v = (*env)->GetStringUTFChars(env, vertex, NULL);
+	f = (*env)->GetStringUTFChars(env, fragment, NULL);
+
+	jint ret = aglLoadShader(v, f);
+
+	(*env)->ReleaseStringUTFChars(env, vertex, v);
+	(*env)->ReleaseStringUTFChars(env, fragment, f);
+
+	return ret;
+}
+
+void Java_dkilian_andy_jni_agl_UseShader(JNIEnv *env, jobject *thiz, jint shader)
+{
+	aglUseShader(shader);
+}
+
+void Java_dkilian_andy_jni_agl_ClearShader(JNIEnv *env, jobject *thiz)
+{
+	aglClearShader();
+}
+
+void Java_dkilian_andy_jni_agl_DeleteShader(JNIEnv *env, jobject *thiz, jint shader)
+{
+	aglDeleteShader(shader);
+}
+
+jint Java_dkilian_andy_jni_agl_CreateTexture(JNIEnv *env, jobject *thiz, jint w, jint h)
+{
+	return aglCreateTexture(w, h);
+}
+
+jint Java_dkilian_andy_jni_agl_CreateEmptyTexture(JNIEnv *env, jobject *thiz)
+{
+	return aglCreateEmptyTexture();
+}
+
+void Java_dkilian_andy_jni_agl_BindTexture(JNIEnv *env, jobject *thiz, jint tex)
+{
+	aglBindTexture(tex);
+}
+
+void Java_dkilian_andy_jni_agl_UnbindTexture(JNIEnv *env, jobject *thiz)
+{
+	aglUnbindTexture();
+}
+
+void Java_dkilian_andy_jni_agl_DeleteTexture(JNIEnv *env, jobject *thiz, jint tex)
+{
+	aglDeleteTexture(tex);
+}
+
+void Java_dkilian_andy_jni_agl_TexturedQuad(JNIEnv *env, jobject *thiz)
+{
+	aglTexturedQuad();
+}
+
+void Java_dkilian_andy_jni_agl_DrawBitmap(JNIEnv *env, jobject *thiz, jint tex)
+{
+	aglDrawBitmap(tex);
+}
+
+void Java_dkilian_andy_jni_agl_DrawBitmapTranslated(JNIEnv *env, jobject *thiz, jint tex, jfloat x, jfloat y)
+{
+	aglDrawBitmapTranslated(tex, x, y);
+}
+
+void Java_dkilian_andy_jni_agl_DrawBitmapTransformed(JNIEnv *env, jobject *thiz, jint tex, jfloat x, jfloat y, jfloat rot, jfloat xscale, jfloat yscale)
+{
+	aglDrawBitmapTransformed(tex, x, y, rot, xscale, yscale);
+}
+
+void Java_dkilian_andy_jni_agl_DrawBitmapWithShader(JNIEnv *env, jobject *thiz, jint tex, jint shader)
+{
+	aglDrawBitmapWithShader(tex, shader);
+}
+
+void Java_dkilian_andy_jni_agl_DrawBitmapWithShaderTranslated(JNIEnv *env, jobject *thiz, jint tex, jint shader, jfloat x, jfloat y)
+{
+	aglDrawBitmapWithShaderTranslated(tex, shader, x, y);
+}
+
+void Java_dkilian_andy_jni_agl_DrawBitmapWithShaderTransformed(JNIEnv *env, jobject *thiz, jint tex, jint shader, jfloat x, jfloat y, jfloat rot, jfloat xscale, jfloat yscale)
+{
+	aglDrawBitmapWithShaderTransformed(tex, shader, x, y, rot, xscale, yscale);
+}
+
+void Java_dkilian_andy_jni_agl_DrawBitmapWithoutShader(JNIEnv *env, jobject *thiz, jint tex)
+{
+	aglDrawBitmapWithoutShader(tex);
+}
+
+void Java_dkilian_andy_jni_agl_DrawBitmapWithoutShaderTranslated(JNIEnv *env, jobject *thiz, jint tex, jfloat x, jfloat y)
+{
+	aglDrawBitmapWithoutShaderTranslated(tex, x, y);
+}
+
+void Java_dkilian_andy_jni_agl_DrawBitmapWithoutShaderTransformed(JNIEnv *env, jobject *thiz, jint tex, jfloat x, jfloat y, jfloat rot, jfloat xscale, jfloat yscale)
+{
+	aglDrawBitmapWithoutShaderTransformed(tex, x, y, rot, xscale, yscale);
+}
+
+void Java_dkilian_andy_jni_agl_ClearColor(JNIEnv *env, jobject *thiz, jfloat r, jfloat g, jfloat b)
+{
+	aglClearColor(r, g, b);
+}
+
+void Java_dkilian_andy_jni_agl_BeginFrame(JNIEnv *env, jobject *thiz)
+{
+	aglBeginFrame();
+}
+
+void Java_dkilian_andy_jni_agl_EndFrame(JNIEnv *env, jobject *thiz)
+{
+	aglEndFrame();
+}
+
+jint Java_dkilian_andy_jni_agl_CreateFBO(JNIEnv *env, jobject *thiz, jint w, jint h)
+{
+	return aglCreateFBO(w, h);
+}
+
+void Java_dkilian_andy_jni_agl_AttachToFBO(JNIEnv *env, jobject *thiz, jint fbo, jint tex)
+{
+	aglAttachToFBO(fbo, tex);
+}
+
+void Java_dkilian_andy_jni_agl_DeleteFBO(JNIEnv *env, jobject *thiz, jint fbo)
+{
+	aglDeleteFBO(fbo);
+}
+
+void Java_dkilian_andy_jni_agl_BeginOffscreenRender(JNIEnv *env, jobject *thiz, jint fbo)
+{
+	aglBeginOffscreenRender(fbo);
+}
+
+void Java_dkilian_andy_jni_agl_EndOffscreenRender(JNIEnv *env, jobject *thiz)
+{
+	aglEndOffscreenRender();
+}
--- AndyNative/src/dkilian/andy/jni/agl.java
+++ AndyNative/src/dkilian/andy/jni/agl.java
@@ -10,9 +10,247 @@ package dkilian.andy.jni;
  * @author dkilian
  */
 public class agl 
-{
+{	
+	/**
+	 * Initializes Andy's native OpenGL renderer in 2D rendering mode
+	 * @param w The width of the screen in virtual coordinates
+	 * @param h The height of the screen in virtual coordinates
+	 */
 	public static native void Initialize2D(int w, int h);
 	
-	// TODO: everything else :)
-	// TODO: and documentation
+	/**
+	 * Sets the dimensions of the screen in virtual coordinates
+	 * @param w The width of the screen in virtual coordinates
+	 * @param h The height of the screen in virtual coordinates
+	 */
+	public static native void SetVirtualDimensions(int w, int h);
+	
+	/**
+	 * Gets the transformation from virtual coordinates to screen coordinates
+	 * @param m A 16-element array to receive the 4x4 transformation matrix
+	 */
+	public static native void GetVirtualTransform(float[] m);
+	
+	/**
+	 * Sets the transformation from virtual coordinates to screen coordinates
+	 * @param m A 16-element array containing the 4x4 transformation matrix
+	 */
+	public static native void SetVirtualTransform(float[] m);
+	
+	/**
+	 * Compute the transformation from virtual space to screen space using the virtual
+	 * coordinates specified in a previous aglInitialize2D() or aglSetVirtualDimensions() call
+	 */
+	public static native void ComputeVirtualTransform();
+	
+	/**
+	 * Compiles a shader program
+	 * @param vertex The source code of the vertex shader
+	 * @param fragment The source code of the fragment shader
+	 * @return A handle to the shader, or zero if an error occurred
+	 */
+	public static native int  LoadShader(String vertex, String fragment);
+	
+	/**
+	 * Sets the given shader as the active shader, which will be used in subsequent rendering operations
+	 * @param shader A handle to the shader to make active
+	 */
+	public static native void UseShader(int shader);
+	
+	/**
+	 * Clears the current shader, marking no shader as active
+	 */
+	public static native void ClearShader();
+	
+	/**
+	 * Deletes a shader, invalidating it and freeing resources it uses
+	 * @param shader A handle to the shader to delete
+	 */
+	public static native void DeleteShader(int shader);
+	
+	/**
+	 * Creates a texture. This texture will have color data, but the data inside the texture is undefined.
+	 * The return value from this function may safely be passed to aglAttachToFBO(). The texture will remain
+	 * bound when this function returns.
+	 * @param w The width of the texture, in pixels
+	 * @param h The height of the texture, in pixels
+	 * @return A handle to the texture
+	 */
+	public static native int  CreateTexture(int w, int h);
+	
+	/**
+	 * Creates a texture with no attached data. Use GLUtils.texImage2D() to fill the texture with color data.
+	 * @return A handle to the texture created
+	 */
+	public static native int  CreateEmptyTexture();
+	
+	/**
+	 * Binds the given texture, setting it as the currently active texture. As a side effect, this method
+	 * unbinds any other texture that is already bound.
+	 * @param tex A handle to the texture to bind
+	 */
+	public static native void BindTexture(int tex);
+	
+	/**
+	 * Unbinds any texture that is currently bound.
+	 */
+	public static native void UnbindTexture();
+	
+	/**
+	 * Deletes a texture, invalidating its handle and freeing all resources it is using
+	 * @param tex A handle to the texture to free
+	 */
+	public static native void DeleteTexture(int tex);
+	
+	/**
+	 * Draws a textured quad, from (0, 0, 0, 1) to (1, 1, 0, 1), using the current ModelView matrix.
+	 */
+	public static native void TexturedQuad();
+	
+	/**
+	 * Draws a bitmap sprite as a textured quad. 
+	 * The current ModelView matrix will be used as the current transformation.
+	 * The currently bound shader will be used, if applicable.
+	 * @param tex The texture containing the bitmap data to draw
+	 */
+	public static native void DrawBitmap(int tex);
+	
+	/**
+	 * Draws a bitmap sprite as a textured quad. 
+	 * The supplied translation will be post-concatenated with the current ModelView matrix.
+	 * The currently bound shader will be used, if applicable.
+	 * @param tex The texture containing the bitmap data to draw
+	 * @param x The amount to translate the sprite, in virtual coordinates
+	 * @param y The amount to translate the sprite, in virtual coordinates
+	 */
+	public static native void DrawBitmapTranslated(int tex, float x, float y);
+
+	/**
+	 * Draws a bitmap sprite as a textured quad. 
+	 * The supplied transformations will be post-concatenated with the current ModelView matrix.
+	 * The currently bound shader will be used, if applicable.
+	 * @param tex The texture containing the bitmap data to draw
+	 * @param x The amount to translate the sprite, in virtual coordinates
+	 * @param y The amount to translate the sprite, in virtual coordinates
+	 * @param rot The amount to rotate the sprite, in degrees
+	 * @param xscale The horizontal scale factor
+	 * @param yscale The vertical scale factor
+	 */
+	public static native void DrawBitmapTransformed(int tex, float x, float y, float rot, float xscale, float yscale);
+
+	/**
+	 * Draws a bitmap sprite as a textured quad. 
+	 * The current ModelView matrix will be used as the current transformation.
+	 * The given shader will be bound, in place of any previously bound shader (if applicable).
+	 * @param tex The texture containing the bitmap data to draw
+	 * @param shader The shader to use to render the sprite
+	 */
+	public static native void DrawBitmapWithShader(int tex, int shader);
+
+	/**
+	 * Draws a bitmap sprite as a textured quad. 
+	 * The supplied translation will be post-concatenated with the current ModelView matrix.
+	 * The given shader will be bound, in place of any previously bound shader (if applicable).
+	 * @param tex The texture containing the bitmap data to draw
+	 * @param shader The shader to use to render the sprite
+	 * @param x The amount to translate the sprite, in virtual coordinates
+	 * @param y The amount to translate the sprite, in virtual coordinates
+	 */
+	public static native void DrawBitmapWithShaderTranslated(int tex, int shader, float x, float y);
+
+	/**
+	 * Draws a bitmap sprite as a textured quad. 
+	 * The supplied transformations will be post-concatenated with the current ModelView matrix.
+	 * The given shader will be bound, in place of any previously bound shader (if applicable).
+	 * @param tex The texture containing the bitmap data to draw
+	 * @param shader The shader to use to render the sprite
+	 * @param x The amount to translate the sprite, in virtual coordinates
+	 * @param y The amount to translate the sprite, in virtual coordinates
+	 * @param rot The amount to rotate the sprite, in degrees
+	 * @param xscale The horizontal scale factor
+	 * @param yscale The vertical scale factor
+	 */
+	public static native void DrawBitmapWithShaderTransformed(int tex, int shader, float x, float y, float rot, float xscale, float yscale);
+
+	/**
+	 * Draws a bitmap sprite as a textured quad. 
+	 * The current ModelView matrix will be used as the current transformation.
+	 * Any currently bound shader will be unbound first.
+	 * @param tex The texture containing the bitmap data to draw
+	 */
+	public static native void DrawBitmapWithoutShader(int tex);
+
+	/**
+	 * Draws a bitmap sprite as a textured quad. 
+	 * The supplied translation will be post-concatenated with the current ModelView matrix.
+	 * Any currently bound shader will be unbound first.
+	 * @param tex The texture containing the bitmap data to draw
+	 * @param x The amount to translate the sprite, in virtual coordinates
+	 * @param y The amount to translate the sprite, in virtual coordinates
+	 */
+	public static native void DrawBitmapWithoutShaderTranslated(int tex, float x, float y);
+
+	/**
+	 * Draws a bitmap sprite as a textured quad. 
+	 * The supplied transformations will be post-concatenated with the current ModelView matrix.
+	 * Any currently bound shader will be unbound first.
+	 * @param tex The texture containing the bitmap data to draw
+	 * @param x The amount to translate the sprite, in virtual coordinates
+	 * @param y The amount to translate the sprite, in virtual coordinates
+	 * @param rot The amount to rotate the sprite, in degrees
+	 * @param xscale The horizontal scale factor
+	 * @param yscale The vertical scale factor
+	 */
+	public static native void DrawBitmapWithoutShaderTransformed(int tex, float x, float y, float rot, float xscale, float yscale);
+	
+	/**
+	 * Sets the color the backbuffer is cleared to at the beginning of every frame
+	 * @param r The red component of the backbuffer clear color, in [0, 1]
+	 * @param g The green component of the backbuffer clear color, in [0, 1]
+	 * @param b The blue component of the backbuffer clear color, in [0, 1]
+	 */
+	public static native void ClearColor(float r, float g, float b);
+	
+	/**
+	 * Does all initialization needed to draw a single frame. Should be called at the beginning of each draw().
+	 */
+	public static native void BeginFrame();
+	
+	/**
+	 * Does all cleanup neededafter drawing a single frame. Should be called at the end of each draw()
+	 */
+	public static native void EndFrame();
+	
+	/**
+	 * Creates a new frame buffer object
+	 * @param w The width of the FBO, in pixels
+	 * @param h The height of the FBO, in pixels
+	 * @return A handle to the FBO created
+	 */
+	public static native int  CreateFBO(int w, int h);
+	
+	/**
+	 * Attaches a color texture to an FBO. See aglCreateTexture().
+	 * @param fbo A handle to the framebuffer object to attach the color buffer to
+	 * @param tex A handle to the color buffer to attach to the framebuffer object
+	 */
+	public static native void AttachToFBO(int fbo, int tex);
+	
+	/**
+	 * Deletes an FBO and its attachments, invalidating them and releasing resources they are currently using
+	 * @param fbo A handle to the FBO to delete
+	 */
+	public static native void DeleteFBO(int fbo);
+	
+	/**
+	 * Begins redirecting render calls to a framebuffer object
+	 * @param fbo A handle to the framebuffer object to render to
+	 */
+	public static native void BeginOffscreenRender(int fbo);
+	
+	/**
+	 * Stops redirecting render calls to a framebuffer object and makes it possible for the framebuffer object's
+	 * color buffer(s) to be read back / rendered like any other texture.
+	 */
+	public static native void EndOffscreenRender();
 }

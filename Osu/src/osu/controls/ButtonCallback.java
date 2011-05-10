b418a8190b6737403cb2b1fa1339effa97a91d4f
Dave Kilian <dave@fracture-studios.com>
2011-05-10 12:28:14 -0400

    Commenting

--- Osu/src/osu/controls/ButtonCallback.java
+++ Osu/src/osu/controls/ButtonCallback.java
@@ -2,7 +2,17 @@ package osu.controls;
 
 import osu.game.HOButton;
 
+/**
+ * Interface for an object that can receive notifications of interaction with an osu.controls.Button
+ * 
+ * @author dkilian
+ */
 public interface ButtonCallback 
 {
+	/**
+	 * Notifies this object of a button interaction
+	 * @param sender The button which was interacted with
+	 * @param event The osu! event corresponding to the button that is sending this notification
+	 */
 	public void buttonEvent(Button sender, HOButton event);
 }
